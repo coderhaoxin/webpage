fa5dba62e454b1a20f95356d88700aa73038b613
haoxin <coderhaoxin@outlook.com>
2013-08-28 08:56:36 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,17 @@
-var q = require('./index');
+var select = require('./index').select;
+var update = require('./index').update;
 
-describe('test mongo-sql-select', function () {
+describe('select', function () {
   it('1', function () {
-    var sql = q('user_table', '*');
+    var sql = select('user_table', '*');
     console.log(sql);
   })
   it('2', function () {
-    var sql = q('user_table', ['name', 'age', 'point', 'create_time']);
+    var sql = select('user_table', ['name', 'age', 'point', 'create_time']);
     console.log(sql);
   })
   it('3', function () {
-    var sql = q('user_table', {
+    var sql = select('user_table', {
       name: 'name',
       age: 'age',
       point: 'point',
@@ -19,19 +20,19 @@ describe('test mongo-sql-select', function () {
     console.log(sql);
   })
   it('4', function () {
-    var sql = q('user_table', ['name', 'age', 'point', 'create_time'], {
+    var sql = select('user_table', ['name', 'age', 'point', 'create_time'], {
       limit: '0,30'
     });
     console.log(sql);
   })
   it('5', function () {
-    var sql = q('user_table', ['name', 'age', 'point', 'create_time'], {
+    var sql = select('user_table', ['name', 'age', 'point', 'create_time'], {
       group: 'name'
     });
     console.log(sql);
   })
   it('6', function () {
-    var sql = q('user_table', ['name', 'age', 'point', 'create_time'], {
+    var sql = select('user_table', ['name', 'age', 'point', 'create_time'], {
       where: {
         'name': '="hao"',
         'age': 'between 12 and 23',
@@ -41,10 +42,37 @@ describe('test mongo-sql-select', function () {
     console.log(sql);
   })
   it('7', function () {
-    var sql = q('user_table', ['name', 'age', 'point', 'create_time'], {
+    var sql = select('user_table', ['name', 'age', 'point', 'create_time'], {
       order: {
         age: 'asc',
         point: 'desc'
+      }
+    });
+    console.log(sql);
+  })
+})
+
+describe('update', function () {
+  it('1', function () {
+    var sql = update('user_table', {
+      name: 'hao',
+      age: 22,
+      point: 1200,
+      create_time: '2013-8-26'
+    });
+    console.log(sql);
+  })
+  it('2', function () {
+    var sql = update('user_table', {
+      name: 'xin',
+      age: 23,
+      point: 1300,
+      create_time: '2013-8-27'
+    }, {
+      where: {
+        'name': '="hao"',
+        'age': 'between 12 and 23',
+        'point': '> 1200'
       }
     });
     console.log(sql);
